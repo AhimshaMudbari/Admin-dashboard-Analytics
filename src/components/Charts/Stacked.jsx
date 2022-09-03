f9af4d2c5ef0bcb46225dba66b8398b0fe2f1aa6
Ahim11 <ahim.mudbari@gmail.com>
2022-09-03 22:13:28 +0545

    bar chart from syncfusion

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,7 +1,42 @@
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from '@syncfusion/ej2-react-charts';
+import {
+  stackedPrimaryYAxis,
+  stackedPrimaryXAxis,
+  stackedCustomSeries,
+} from '../../data/dummy';
 import React from 'react';
 
-const Stacked = () => {
-  return <div>Stacked</div>;
-};
-
+class Stacked extends React.Component {
+  render() {
+    const { height, width } = this.props;
+    return (
+      <ChartComponent
+        height={height}
+        width={width}
+        id="stack-chart"
+        primaryXAxis={stackedPrimaryXAxis}
+        primaryYAxis={stackedPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        legendSettings={{ background: 'white' }}
+      >
+        <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+        <SeriesCollectionDirective>
+          {stackedCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    );
+  }
+}
 export default Stacked;
