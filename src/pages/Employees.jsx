cbfe77db63e6db1d87377aae561393776106aafa
Ahim11 <ahim.mudbari@gmail.com>
2022-09-03 22:16:37 +0545

    Employee component with data and features from syncFusion

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
-
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Page,
+  Search,
+  Toolbar,
+  Inject,
+} from '@syncfusion/ej2-react-grids';
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
 const Employees = () => {
-  return <div>Employees</div>;
+  const toolbarOptions = ['Search'];
+  const editing = { allowDeleting: true, allowEditing: true };
+  return (
+    <div className="m-2 mt-24 bg-white md:m-10 p-z md:p-10 rounded-3xl">
+      <Header title="Employee Details" category="Page" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={toolbarOptions}
+        width="auto"
+        editSettings={editing}
+        pageSettings={{ pageCount: 5 }}
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Employees;
