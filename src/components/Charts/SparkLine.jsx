f9d98b729eb13a0979db931865b59aad7c4346ea
Ahim11 <ahim.mudbari@gmail.com>
2022-09-03 22:12:59 +0545

    line chart from syncfusion

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
 
-const SparkLine = () => {
-  return <div>SparkLine</div>;
-};
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from '@syncfusion/ej2-react-charts';
 
+class SparkLine extends React.Component {
+  render() {
+    const { color, currentColor, id, height, width, data, type } = this.props;
+    return (
+      <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        tooltipSettings={{
+          visible: true,
+          // eslint-disable-next-line no-template-curly-in-string
+          format: '${x} : data ${yval}',
+          trackLineSettings: {
+            visible: true,
+          },
+        }}
+        markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+        dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+    );
+  }
+}
+// const SparkLine = ({ color, currentColor, id, height, width, data, type }) => {
+//   return (
+//     <SparklineComponent
+//       id={id}
+//       height={height}
+//       width={width}
+//       lineWidth={1}
+//       valueType="Numeric"
+//       fill={color}
+//       border={{ color: currentColor, width: 2 }}
+//       dataSource={data}
+//       xName="X"
+//       yName="Y"
+//       type={type}
+//     >
+//       <Inject services={[SparklineTooltip]} />
+//     </SparklineComponent>
+//   );
+// };
 export default SparkLine;
+// ReactDOM.render(<SparkLine />, document.getElementById('root'));
