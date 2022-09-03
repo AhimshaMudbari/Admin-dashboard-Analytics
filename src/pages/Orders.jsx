4938c9b5927878546b2cdb22c9336dad62df814f
Ahim11 <ahim.mudbari@gmail.com>
2022-09-03 22:16:07 +0545

    Orders component with data and feattures from syncFusion

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
-
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from '@syncfusion/ej2-react-grids';
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div className="m-2 md:m-10 p-z md:p-10">
+      <Header title="Orders" category="Page" />
+      <GridComponent
+        id="gridComp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Page,
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            ExcelExport,
+            PdfExport,
+            Edit,
+          ]}
+        />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Orders;
