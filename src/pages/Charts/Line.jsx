a05047f6150be75372e5eceba22f1799b58ad7d2
Ahim11 <ahim.mudbari@gmail.com>
2022-09-10 21:14:27 +0545

    Line chart componenet

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
-
+import { LineChart, Header } from '../../components';
 const Line = () => {
-  return <div>Line</div>;
+  return (
+    <div className="p-10 m-4 mt-24 bg-white md:m-10 dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Chart" title="Inflation rate" />
+      <div className="w-full">
+        <LineChart />
+      </div>
+    </div>
+  );
 };
 
 export default Line;
